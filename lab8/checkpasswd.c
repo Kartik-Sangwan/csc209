e5438ab54f9fa029d43aa826a2b7aa7f6eaa1ced
sangwan2 <sangwan2@teach.cs.toronto.edu>
2020-03-05 13:59:50 -0500

    added file

--- lab8/checkpasswd.c
+++ lab8/checkpasswd.c
@@ -31,6 +31,61 @@ int main(void) {
   }
   
   // TODO
+  int n, status;
+  int fd[2];
+  n = fork();
+
+  if (pipe(fd) == -1) {
+        perror("pipe");
+        exit(1);
+  }
+
+  if (n < 0){
+    perror("fork");
+    exit(1);
+  } else if (n == 0){
+
+    if (write(fd[1], user_id, 10) == -1) {
+        perror("write to pipe");
+    } 
+    if (write(fd[1], password, 10) == -1) {
+        perror("write to pipe");
+    } 
+
+    if (dup2(fd[0], fileno(stdin)) == -1) {
+      perror("dup2");
+    }
+
+    close(fd[0]);
+    close(fd[1]);
+    execl("./validate", "validate", NULL);
+
+  } else {
+
+    close(fd[0]);
+    close(fd[1]);
+
+    if (wait(&status) == -1){
+      perror("wait");
+      exit(1);
+    }
+
+    int exitval;
+
+    if(WIFEXITED(status)){
+
+      exitval = WEXITSTATUS(status);
+      
+      if (exitval == 0) printf("%s", SUCCESS);
+      else if (exitval == 2) printf("%s", INVALID);
+      else if (exitval == 3) printf("%s", NO_USER);
+      else{
+        perror("error in parent");
+        exit(1);
+      }
+    }
+
+  }
 
   return 0;
 }
