7b3631f0fdca42a74ca9a1c208f3aa03999ede30
sangwan2 <sangwan2@teach.cs.toronto.edu>
2020-03-16 13:42:41 -0400

    File Added

--- a3/closest.c
+++ a3/closest.c
@@ -9,6 +9,8 @@
 #include "parallel_closest.h"
 
 
+//print_usage is called when there is an error in the command line arguements.
+
 void print_usage() {
     fprintf(stderr, "Usage: closest -f filename -d pdepth\n\n");
     fprintf(stderr, "    -d Maximum process tree depth\n");
@@ -21,13 +23,16 @@ int main(int argc, char **argv) {
     int n = -1;
     long pdepth = -1;
     char *filename = NULL;
+
+	//pcount is the number of worker processes that will be instantiated.
     int pcount = 0;
 	int ch = -1;
-	
-    // TODO: Parse the command line arguments
+
+    // Parsing the command line arguments with flags using getopt.
 	if (argc != 5){
 		print_usage();
 	}
+	// Parsing the flags and storing the parameters.
 	while ((ch = getopt(argc, argv, "f:d:")) != -1){
 
 		switch(ch) {
@@ -57,6 +62,5 @@ int main(int argc, char **argv) {
     // Calculate the result using the parallel algorithm.
     double result_p = closest_parallel(points_arr, n, pdepth, &pcount);
     printf("The smallest distance: is %.2f (total worker processes: %d)\n", result_p, pcount);
-
     exit(0);
 }
