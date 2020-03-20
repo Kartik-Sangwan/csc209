8aa8c1c0c9a5717583a172d4b09ec3927741f2f0
sangwan2 <sangwan2@teach.cs.toronto.edu>
2020-03-20 11:08:01 -0400

    Added Files

--- lab10/bufserver.c
+++ lab10/bufserver.c
@@ -5,7 +5,6 @@
 #include <arpa/inet.h>
 
 #include "socket.h"
-
 #ifndef PORT
   #define PORT 30000
 #endif
@@ -38,10 +37,10 @@ int main() {
         int nbytes;
         while ((nbytes = read(fd, after, room)) > 0) {
             // Step 1: update inbuf (how many bytes were just added?)
-
+			inbuf += nbytes;
+			after = &buf[inbuf];
 
             int where;
-
             // Step 2: the loop condition below calls find_network_newline
             // to determine if a full line has been read from the client.
             // Your next task should be to implement find_network_newline
@@ -56,7 +55,9 @@ int main() {
                 // Be sure to put a '\0' in the correct place first;
                 // otherwise you'll get junk in the output.
 
+                // printf("%d", where);
 
+				buf[where - 2] = '\0';
                 printf("Next message: %s\n", buf);
                 // Note that we could have also used write to avoid having to
                 // put the '\0' in the buffer. Try using write later!
@@ -65,13 +66,15 @@ int main() {
                 // There might be stuff after the line, so don't just do inbuf = 0.
 
                 // You want to move the stuff after the full line to the beginning
-                // of the buffer.  A loop can do it, or you can use memmove.
+                // of the buffer. A loop can do it, or you can use memmove.
                 // memmove(destination, source, number_of_bytes)
-
-
+				inbuf = after - &buf[where];
+				memmove(buf, &buf[where], inbuf);
+				memset(&buf[inbuf], '\0', BUFSIZE - inbuf);
             }
             // Step 5: update after and room, in preparation for the next read.
-
+			after = &buf[inbuf];
+			room = BUFSIZE - inbuf;
 
         }
         close(fd);
@@ -90,5 +93,11 @@ int main() {
  * Definitely do not use strchr or other string functions to search here. (Why not?)
  */
 int find_network_newline(const char *buf, int n) {
+
+	for (int i = 0; i < n - 1; i++){
+		if (buf[i] == '\r' && buf[i + 1] == '\n'){
+			return i + 2;
+		}
+	}
     return -1;
 }
